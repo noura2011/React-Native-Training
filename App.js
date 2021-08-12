adad9c2a87bb96ba7602e1c915bab31bff23747f
noura <nelfrash@gmail.com>
2021-08-13 00:06:52 +0200

    Day-3

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import { HomeScreen } from "./src/screens";
+import RootNavigator from "./src/navigation"
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle={"light-content"} />
-      <HomeScreen />
+      <RootNavigator />
     </>
   );
 }
